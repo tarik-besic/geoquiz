42c14794e81370872c232f5018a034e5269e381e
Tarik Besic <tarik_besic@live.com>
2022-01-10 23:26:51 +0100

    added navigation container

--- geoQuiz/App.js
+++ geoQuiz/App.js
@@ -1,15 +1,42 @@
-import React from "react";
-import { View,Text } from 'react-native'
+import React, { useState } from "react";
+import { View,Text,StyleSheet, Image } from 'react-native'
+import data from "./quizData/flag-data"
+import FlagsScreen from "./src/screens/flags"
+import HomeScreen from "./src/screens/home"
+import MonumentScreen from "./src/screens/monuments";
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const App=()=>{
+
   return(
-    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-      <Text>
-        Hello World
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Flags" component={FlagsScreen} />
+        <Stack.Screen name="Monuments" component={MonumentScreen} />
+      </Stack.Navigator>
+    </NavigationContainer> 
   )
 }
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  questionTitle:{
+    marginVertical:10
+  },
+  image:{
+    width:40,
+    height:40
+  }
+})
+  
 
 export default App;
