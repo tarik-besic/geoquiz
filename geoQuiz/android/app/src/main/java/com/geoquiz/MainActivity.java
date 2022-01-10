8fceb2cc731a32136f84d45edf1b6bd34d5d5a81
Tarik Besic <tarik_besic@live.com>
2022-01-10 23:24:55 +0100

    navigation

--- geoQuiz/android/app/src/main/java/com/geoquiz/MainActivity.java
+++ geoQuiz/android/app/src/main/java/com/geoquiz/MainActivity.java
@@ -1,4 +1,5 @@
 package com.geoquiz;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 
@@ -12,4 +13,8 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "geoQuiz";
   }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
 }
