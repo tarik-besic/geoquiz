9736dc71998f913d4dea188f7bed00ec3c19d776
Tarik Besic <tarik_besic@live.com>
2022-01-16 23:28:14 +0100

    all images for countries added

--- geoQuiz/quizData/flag-data.js
+++ geoQuiz/quizData/flag-data.js
@@ -1,47 +1,213 @@
-export default data=[
-    {
-        id:0,
+export default data = [{
+        id: 0,
         question: "Argentina",
-        correctOptionId:3,
-        options:[
-            {
-                optionId:0,
-                imgUrl:require("../assets/images/aww.png")
+        correctOptionId: 3,
+        options: [{
+                optionId: 0,
+                imgUrl: require("../assets/images/aww.png")
             },
             {
-                optionId:1,
-                imgUrl:require("../assets/images/greece.png")
+                optionId: 1,
+                imgUrl: require("../assets/images/greece.png")
             },
             {
-                optionId:2,
-                imgUrl:require("../assets/images/bww.png")
+                optionId: 2,
+                imgUrl: require("../assets/images/bww.png")
             },
             {
-                optionId:3,
-                imgUrl:require("../assets/images/ar.png")
+                optionId: 3,
+                imgUrl: require("../assets/images/ar.png")
             }
         ]
     },
     {
-        id:1,
+        id: 1,
         question: "USA",
-        correctOptionId:2,
-        options:[
-            {
-                optionId:0,
-                imgUrl:require("../assets/images/lr.png")
+        correctOptionId: 2,
+        options: [{
+                optionId: 0,
+                imgUrl: require("../assets/images/lr.png")
             },
             {
-                optionId:1,
-                imgUrl:require("../assets/images/my.png")
+                optionId: 1,
+                imgUrl: require("../assets/images/my.png")
             },
             {
-                optionId:2,
-                imgUrl:require("../assets/images/us.png")
+                optionId: 2,
+                imgUrl: require("../assets/images/us.png")
             },
             {
-                optionId:3,
-                imgUrl:require("../assets/images/xk.png")
+                optionId: 3,
+                imgUrl: require("../assets/images/xk.png")
+            }
+        ]
+    },
+    {
+        id: 2,
+        question: 'Bosnia and Herzegovina',
+        correctOptionId: 2,
+        options: [{
+                optionId: 0,
+                imgUrl: require("../assets/images/aww.png")
+            },
+            {
+                optionId: 1,
+                imgUrl: require("../assets/images/cx.png")
+            },
+            {
+                optionId: 2,
+                imgUrl: require("../assets/images/ba.png")
+            },
+            {
+                optionId: 3,
+                imgUrl: require("../assets/images/tokel.png")
+            }
+        ]
+    }, {
+        id: 3,
+        question: 'Australia',
+        correctOptionId: 3,
+        options: [{
+                optionId: 0,
+                imgUrl: require("../assets/images/fi.png")
+            },
+            {
+                optionId: 1,
+                imgUrl: require("../assets/images/sv.png")
+            },
+            {
+                optionId: 2,
+                imgUrl: require("../assets/images/nz.png")
+            },
+            {
+                optionId: 3,
+                imgUrl: require("../assets/images/au.png")
+            }
+        ]
+    }, {
+        id: 4,
+        question: 'Peru',
+        correctOptionId: 0,
+        options: [{
+                optionId: 0,
+                imgUrl: require("../assets/images/pe.png")
+            },
+            {
+                optionId: 1,
+                imgUrl: require("../assets/images/ye.png")
+            },
+            {
+                optionId: 2,
+                imgUrl: require("../assets/images/ca.png")
+            },
+            {
+                optionId: 3,
+                imgUrl: require("../assets/images/pl.png")
+            }
+        ]
+    } ,{
+        id: 5,
+        question: 'Brazil',
+        correctOptionId: 1,
+        options: [{
+                optionId: 0,
+                imgUrl: require("../assets/images/in.png")
+            },
+            {
+                optionId: 1,
+                imgUrl: require("../assets/images/br.png")
+            },
+            {
+                optionId: 2,
+                imgUrl: require("../assets/images/ng.png")
+            },
+            {
+                optionId: 3,
+                imgUrl: require("../assets/images/mx.png")
+            }
+        ]
+    }, {
+        id: 6,
+        question: 'Georgia',
+        correctOptionId: 0,
+        options: [{
+                optionId: 0,
+                imgUrl: require("../assets/images/ge.png")
+            },
+            {
+                optionId: 1,
+                imgUrl: require("../assets/images/gg.png")
+            },
+            {
+                optionId: 2,
+                imgUrl: require("../assets/images/gb-eng.png")
+            },
+            {
+                optionId: 3,
+                imgUrl: require("../assets/images/ch.png")
+            }
+        ]
+    }, {
+        id: 7,
+        question: "Japan",
+        correctOptionId: 1,
+        options: [{
+                optionId: 0,
+                imgUrl: require("../assets/images/kr.png")
+            },
+            {
+                optionId: 1,
+                imgUrl: require("../assets/images/jp.png")
+            },
+            {
+                optionId: 2,
+                imgUrl: require("../assets/images/je.png")
+            },
+            {
+                optionId: 3,
+                imgUrl: require("../assets/images/gl.png")
+            }
+        ]
+    }, {
+        id: 8,
+        question: 'Palestina',
+        correctOptionId: 1,
+        options: [{
+                optionId: 0,
+                imgUrl: require("../assets/images/sd.png")
+            },
+            {
+                optionId: 1,
+                imgUrl: require("../assets/images/ps.png")
+            },
+            {
+                optionId: 2,
+                imgUrl: require("../assets/images/jo.png")
+            },
+            {
+                optionId: 3,
+                imgUrl: require("../assets/images/ae.png")
+            }
+        ]
+    }, {
+        id: 9,
+        question: 'Slovenia',
+        correctOptionId: 3,
+        options: [{
+                optionId: 0,
+                imgUrl: require("../assets/images/paragv.png")
+            },
+            {
+                optionId: 1,
+                imgUrl: require("../assets/images/russi.png")
+            },
+            {
+                optionId: 2,
+                imgUrl: require("../assets/images/slovak.png")
+            },
+            {
+                optionId: 3,
+                imgUrl: require("../assets/images/slov.png")
             }
         ]
     }
