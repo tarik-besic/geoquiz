bba7f8b187e0e214ebdd58f1f1422ecc175d56c6
Tarik Besic <tarik_besic@live.com>
2022-01-17 15:01:53 +0100

    added end game modal and displaying score

--- geoQuiz/src/screens/flags.js
+++ geoQuiz/src/screens/flags.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, StyleSheet, View , Text, Image, TouchableOpacity} from "react-native";
+import { Button, StyleSheet, View , Text, Image, TouchableOpacity, SafeAreaView, Modal} from "react-native";
 
 //data
 import data from "../../quizData/flag-data";
@@ -8,42 +8,37 @@ import data from "../../quizData/flag-data";
 import correctCircle from "../../assets/images/circleC.png"
 import wrongCircle from "../../assets/images/circleW.png"
 
-const Flags=()=>{
+const Flags=({navigation})=>{
 
     const [question,setQuestion]=useState(0);
     const [showButton,setShowButton]=useState(0);
     const [score,setScore]=useState(0);
-    const [correctOption,setCorrectOption]=useState(null);
+    const [correctOption,setCorrectOption]=useState(data[question].correctOptionId);
     const [currentOptionSelected,setCurrentOptionSelected]=useState(null);
     const [btnDisabled,setBtnDisabled]=useState(false); //if user can select options 
-
+    const [showModal,setShowModal]=useState(false);
     const validation=(id)=>{
             setBtnDisabled(true);
-            setCorrectOption(data[question].correctOptionId)
             setCurrentOptionSelected(id)
             //validating correct option
             if(id==correctOption)
                 {
                     setScore(score+1);
-                    setShowButton(true);
-                    console.log("BRAVOOOOOOOOOOO")
+                    console.log("You guessed it")
                 }
-            else
-                {
-                    setShowButton(true);
-                }
+            setShowButton(true);
         }
 
     const nextQuestion=()=>{
         if(data[question].id==data.length-1)
             {
                 //last question
-                console.log("NEMA VISE PITANJA")
-                return null
+                setShowModal(true);
+                
             }
         else{
             setQuestion(question+1);
-            setCorrectOption(null); 
+            setCorrectOption(data[question+1].correctOptionId); 
             setCurrentOptionSelected(null);
             setShowButton(false);
             setBtnDisabled(false)
@@ -51,51 +46,90 @@ const Flags=()=>{
         }
     }
     const renderQuestion=(id)=>{
-            console.log("renderan"+id);
-            let displayImg;
         return(
-            <View >
-                <Text style={{textAlign:"center", fontSize:30, marginBottom:30}}>
-                    Guess the Country
-                </Text>
+                <View style={{ backgroundColor: '#142850',height:"100%"}}>
+                    <View style={{flexDirection:"row", justifyContent:"space-between"}}>
+                        <Text style={{marginLeft:5, fontSize:24, color:"#22AE0B"}}>Score: {score}</Text> 
+                        <Text style={{marginRight:5, fontSize:24, color:"#ffffff"}}>{question+1}/ {data.length}</Text>
+                    </View>
+                    <View >
+                        <Text style={{textAlign:"center", fontSize:30,color:"#fff",marginVertical:30}}>
+                            Guess the Country
+                        </Text>
 
-                <View >
-                    <Text style={
-                        {textAlign:"center",
-                        fontSize:30,
-                        marginBottom:30,
-                        fontWeight:"bold",
-                        color: currentOptionSelected==data[question].correctOptionId ? "#49FF00" : btnDisabled ? "#D72323" : "#c4c4c4"
-                        }} >
-                        {data[question].question}
-                    </Text>
-                    <View style={{justifyContent:"center",alignItems:"center",marginBottom:25}}>
-                    {currentOptionSelected==data[question].correctOptionId ? <Image source={correctCircle}/> : currentOptionSelected!=null ? <Image source={wrongCircle}/> : null}
+                        <View >
+                            <Text style={
+                                {textAlign:"center",
+                                fontSize:30,
+                                marginBottom:30,
+                                fontWeight:"bold",
+                                color: currentOptionSelected==data[question].correctOptionId ? "#49FF00" : btnDisabled ? "#D72323" : "#c4c4c4"
+                                }} >
+                                {data[question].question}
+                            </Text>
+                            <View style={{justifyContent:"center",alignItems:"center",marginBottom:25}}>
+                            {currentOptionSelected==data[question].correctOptionId ? <Image source={correctCircle}/> : currentOptionSelected!=null ? <Image source={wrongCircle}/> : null}
+                            </View>
+                        </View>
+
+                        <View style={{flexDirection:"row",flexWrap:"wrap",justifyContent:"space-evenly"}}>
+                            {data[question].options.map(option=>{
+                                return(
+                                    <TouchableOpacity 
+                                    key={option.optionId}
+                                    onPress={()=>{
+                                        validation(option.optionId) //answer validation
+                                    }}
+                                    disabled={btnDisabled} //setting to disabled if select i disabled
+                                    style={{
+                                        marginVertical:5,
+                                        borderWidth:4,
+                                        borderColor: showButton ? (option.optionId==correctOption) ? "#49FF00" : option.optionId==currentOptionSelected ? "#D72323" : 0 : 0
+                                    }}
+                                    >
+                                        <Image source={option.imgUrl} style={{width: 180, height:110}} />
+                                    </TouchableOpacity>
+                                )
+                            })}
+                        </View>
+                        {renderButton()}
+                        {showModal &&
+                              <Modal visible={true}>
+                                <View style={{flex:1,justifyContent:"center",alignItems:"center",backgroundColor:"#142850"}}>
+                                    <View style={{width:350,height:250,backgroundColor:"#fff",borderRadius:20,justifyContent:"center",alignItems:"center"}}>
+                                        <Text style={{fontSize:25,fontWeight:"bold",color:"#000"}}>
+                                            {score > 4 ? "Congratulations!" : "Better luck next time"}
+                                        </Text>
+                                        <Text style={{
+                                            fontSize:30,
+                                            marginVertical:25,
+                                            color: score>4 ? "#10D610" : "red"
+                                            
+                                            }}>{score} / {data.length}</Text>
+                                    <TouchableOpacity 
+                                        onPress={()=>{
+                                            navigation.navigate("Home")
+                                        }}
+                                        style={{
+                                            backgroundColor:"#4EB4D1",
+                                            width:200,
+                                            height:60,
+                                            borderRadius:20,
+                                            justifyContent:"center",
+                                            alignItems:"center"
+                                        
+                                        }}
+                                        >
+                                        <Text style={{fontSize:25,color:"#2d2d2d"}}>Continue</Text>
+                                    </TouchableOpacity>
+                                    </View>
+                                    
+                                </View>
+                                
+                                </Modal>
+                        }
                     </View>
                 </View>
-
-                <View style={{flexDirection:"row",flexWrap:"wrap",justifyContent:"space-evenly"}}>
-                    {data[question].options.map(option=>{
-                        return(
-                            <TouchableOpacity 
-                            key={option.optionId}
-                            onPress={()=>{
-                                validation(option.optionId) //answer validation
-                            }}
-                            disabled={btnDisabled} //setting to disabled if select i disabled
-                            style={{
-                                marginVertical:5,
-                                borderWidth:4,
-                                borderColor: (option.optionId==correctOption) ? "#49FF00" : option.optionId==currentOptionSelected ? "#D72323" : 0
-                            }}
-                            >
-                                <Image source={option.imgUrl} style={{width: 180, height:110}} />
-                            </TouchableOpacity>
-                        )
-                    })}
-                </View>
-                {renderButton()}
-            </View>
         )
     }
 
