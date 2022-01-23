e4e00d0565ce4d17ae958109ef983ac183d2aa60
Tarik Besic <tarik_besic@live.com>
2022-01-23 18:57:14 +0100

    monument data added

--- geoQuiz/quizData/monument-data.js
+++ geoQuiz/quizData/monument-data.js
@@ -1,49 +1,109 @@
-export default data={
-    staticQuestionName:"What's the name of this monument",
-    questions:[
-        {
-            id:0,
-            imgUrl: "imgurl1",
-            correctOptionId:0,
-            options:[
-                {
-                    optionId:0,
-                    name:"Burj Khalifa"
+export default data = {
+    staticQuestionName: "What's the name of this monument",
+    questions: [{
+            imgUrl: require("../assets/images/burj.png"),
+            correctOptionId: 0,
+            options: [{
+                    optionId: 0,
+                    name: "Burj Khalifa"
                 },
                 {
-                    optionId:1,
-                    name:"Big Ben"
+                    optionId: 1,
+                    name: "Big Ben"
                 },
                 {
-                    optionId:2,
-                    name:"Machu Pichu"
+                    optionId: 2,
+                    name: "Machu Pichu"
                 },
                 {
-                    optionId:3,
-                    name:"White House"
+                    optionId: 3,
+                    name: "White House"
                 }
             ]
         },
         {
-            id:1,
-            imgUrl: "imgurl1",
-            correctOptionId:2,
-            options:[
-                {
-                    optionId:1,
-                    name:"Big Ben"
+            imgUrl: require("../assets/images/tajMahal.png"),
+            correctOptionId: 3,
+            options: [{
+                    optionId: 0,
+                    name: "Big Ben"
                 },
                 {
-                    optionId:0,
-                    name:"Burj Khalifa"
+                    optionId: 1,
+                    name: "Burj Khalifa"
                 },
                 {
-                    optionId:3,
-                    name:"White House"
+                    optionId: 2,
+                    name: "White House"
                 },
                 {
-                    optionId:2,
-                    name:"Machu Pichu"
+                    optionId: 3,
+                    name: "Taj Mahal"
+                }
+            ]
+        },
+        {
+            imgUrl: require("../assets/images/white_house.png"),
+            correctOptionId: 2,
+            options: [{
+                    optionId: 0,
+                    name: "Colosseum"
+                },
+                {
+                    optionId: 1,
+                    name: "Pentagon"
+                },
+                {
+                    optionId: 2,
+                    name: "White House"
+                },
+                {
+                    optionId: 3,
+                    name: "Tower of London"
+
+                }
+            ]
+        },
+        {
+            imgUrl: require("../assets/images/bigBen.png"),
+            correctOptionId: 1,
+            options: [{
+                    optionId: 0,
+                    name: "Peace Tower"
+                },
+                {
+                    optionId: 1,
+                    name: "Big Ben"
+                },
+                {
+                    optionId: 2,
+                    name: "Zimmer Tower"
+                },
+                {
+                    optionId: 3,
+                    name: "Rajabai Clock Tower"
+
+                }
+            ]
+        },
+        {
+            imgUrl: require("../assets/images/notre.png"),
+            correctOptionId: 3,
+            options: [{
+                    optionId: 0,
+                    name: "Sydney Opera House"
+                },
+                {
+                    optionId: 1,
+                    name: "Rajabai Clock Tower"
+                },
+                {
+                    optionId: 2,
+                    name: "Sphinx"
+                },
+                {
+                    optionId: 3,
+                    name: "Notre Dame"
                 }
             ]
         }
