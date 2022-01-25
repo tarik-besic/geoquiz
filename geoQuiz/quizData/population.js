70f3e31818cd73a7a474b52fb964cb4b65205df1
Tarik Besic <tarik_besic@live.com>
2022-01-26 00:32:22 +0100

    added population data

---
+++ geoQuiz/quizData/population.js
@@ -0,0 +1,35 @@
+const data=[
+    {
+        population:"329.5 million",
+        imgUrl: require("../assets/images/flags/us.png")
+    },{
+        population:"125.8 million",
+        imgUrl: require("../assets/images/flags/jp.png")
+    },{
+        population:"212.6 million",
+        imgUrl: require("../assets/images/flags/br.png")
+    },{
+        population:"3.281 million",
+        imgUrl: require("../assets/images/flags/ba.png")
+    },{
+        population:"128.9 million",
+        imgUrl: require("../assets/images/flags/mx.png")
+    },{
+        population:"25.69 million",
+        imgUrl: require("../assets/images/flags/au.png")
+    },{
+        population:"51.78 million",
+        imgUrl: require("../assets/images/flags/kr.png")
+    },{
+        population:"144.1 million",
+        imgUrl: require("../assets/images/flags/russi.png")
+    },{
+        population:"32.97 million",
+        imgUrl: require("../assets/images/flags/pe.png")
+    },{
+        population:"206.1 million",
+        imgUrl: require("../assets/images/flags/ng.png")
+    }
+]
+
+export default data;
