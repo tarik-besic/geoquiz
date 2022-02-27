4473f0669dd6cc1d9c1ea1a673f3cfd401308069
Tarik Besic <tarik_besic@live.com>
2022-02-27 14:49:28 +0100

    new colors

--- geoQuiz/assets/colors/colors.js
+++ geoQuiz/assets/colors/colors.js
@@ -1,11 +1,27 @@
 const COLORS={
-    grenMain:"#BBE0B5",
-    greenDark:"#628758",
-    correct:"#22AE0B",
-    background:"#E3E3E2",
-    yellow:"#dcd668",
-    black:"#393E46",
-    incorrect:"#D72323"
+    MAIN:{
+        green:"#75B289",
+        yellow:"#C4CE63",
+        blue:"#4EB4D1"
+    },
+    FLAGS:{
+        header:"#BBE0B5",
+        headerText:"#628758",
+        correct:"#22AE0B",
+        background:"#E6F5E4",
+        yellow:"#dcd668",
+        black:"#393E46",
+        incorrect:"#D72323"
+    },
+    MONUMENTS:{
+        header:"#D5DCEC",
+        background:"#9BC7D3",
+        headerText:"#B5ADAD",
+        white:"#FFFFFF",
+        heading:"#FFF",
+        wrap:"#D5DCEC",
+        gray:"#8F8F8F"
+    }
 }
 
 export default COLORS;
